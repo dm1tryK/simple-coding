dcdcb1a07722320be32f05495ad9217a96f405af
dm1tryk <dm1tryk.this@gmail.com>
2021-08-20 10:22:20 +0300

    init

---
+++ src/Application.java
@@ -0,0 +1,11 @@
+public class Application {
+    public static void main(String[] args) {
+        int[] digits = new int[] {1,2,3,4,3,2,1};
+        calculator; //todo сделать экземляр объекта калькулятора
+        res;//todo переменная с итоговым результатом вычисления
+        for (int i = 0; i < digits.length; i++) {
+            //todo если число четное, то прибавить его к res, иначе вычесть
+        }
+        System.out.println(res == 0);
+    }
+}
