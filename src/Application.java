cda1ad35d9d9e7c4b186f24eac01fcc467cc6249
FuJloCoF <kuzin-o@mail.ru>
2021-09-02 11:59:36 +0300

    1

--- src/Application.java
+++ src/Application.java
@@ -1,11 +1,39 @@
+import task.calculator.Calculator;
+import java.util.Scanner;
+
+
+
 public class Application {
-    public static void main(String[] args) {
-        int[] digits = new int[] {1,2,3,4,3,2,1};
-        calculator; //todo сделать экземляр объекта калькулятора
-        res;//todo переменная с итоговым результатом вычисления
-        for (int i = 0; i < digits.length; i++) {
-            //todo если число четное, то прибавить его к res, иначе вычесть
+
+    public static void main(String[] args)
+    {
+        Calculator calc = new Calculator();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        double num1 = scan.nextDouble();
+        System.out.println("Введите второе число");
+        double num2 = scan.nextDouble();
+        System.out.println("Введите 0 если нужно выполнить сложение или 1 для вычитания");
+        double num3 = scan.nextDouble();
+        if (num3 ==0)
+        {
+            double res = calc.add(num1 , num2);
+            System.out.println("Сумма чисел = " + res);
         }
-        System.out.println(res == 0);
+        else
+        {
+            double res2 = calc.diff(num1 , num2);
+            System.out.println("разница чисел = " + res2);
+        }
+        double res3 = calc.add(num1 , num2);
+        boolean c = calc.isEven(res3);
+        if (c)
+        {
+            System.out.println("Число получилось четным");
+        }
+        else
+        {
+            System.out.println("Число получилось нечетным");
+        }
     }
 }
--- src/task/calculator/Calculator.java
+++ src/task/calculator/Calculator.java
@@ -5,13 +5,21 @@ package task.calculator;
  * TODO Требуется дописать сигнатуру методов: модификатор доступа, тип возвращаемого значения
  * TODO Требуется написать релизацию по сложению (add), вычитанию (diff) и проверки четности числа (isEven)
  */
-public class Calculator {
-    add(long d1, long d2) {
+public class Calculator
+{
+    public double add (double d1, double d2)
+    {
+       return d1 + d2;
     }
 
-    diff(long d1, long d2) {
+    public double diff(double d1, double d2)
+    {
+        return d1 - d2;
     }
 
-    isEven(long d) {
+    public boolean isEven(double d)
+    {
+        boolean c = d % 2 == 0;
+        return c;
     }
 }
